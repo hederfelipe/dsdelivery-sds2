ad9d1282e98c429c3a2c9ef0276439cb047ed62d
Heder Felipe <hederfelipe@gmail.com>
2021-01-07 15:09:34 -0300

    create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+     <h1><Counter/ ></h1>
+    </div>
+  );
+}
+
+export default App;
