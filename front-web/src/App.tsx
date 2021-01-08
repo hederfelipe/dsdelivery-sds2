c1c31e4cfb3404f413101377af9e8682777c2e55
Heder Felipe <hederfelipe@gmail.com>
2021-01-08 11:29:06 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,11 @@
-
-import logo from './logo.svg';
 import './App.css';
-import Counter from './Counter';
+import Navbar from './Navbar'
+
 
 function App() {
   return (
     <div>
-     <h1><Counter/ ></h1>
+     <Navbar />
     </div>
   );
 }
