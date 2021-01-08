f6b932dd5ad4282548ee0ad17dd44abf04e85586
Heder Felipe <hederfelipe@gmail.com>
2021-01-08 14:34:11 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,20 @@
+import './styles.css';
+import {ReactComponent as YoutubeIcon} from './youtube.svg';
+import {ReactComponent as LinkedingIcon} from './linkedin.svg';
+import {ReactComponent as InstagramIcon} from './instagram.svg';
+
+function Footer(){
+    return(
+    <footer className="main-footer">
+      Desenvolvido por HEDER TECH
+      <div className="footer-icons">
+        <a href="www.youtube.com" target="_new"><YoutubeIcon/></a>
+        <a href="www.linkeding.com" target="_new"><LinkedingIcon/></a>
+        <a href="www.instagram.com" target="_new"><InstagramIcon/></a>
+
+
+      </div>
+    </footer>
+    );
+}
+export default Footer;
